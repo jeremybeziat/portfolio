18bf1e10abd336f27e4ad0ff97305e28126cf5ed
jeremybeziat <jeremybeziat.pro@gmail.com>
2022-12-23 16:54:50 +0100

    refactor : add components

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+function Header() {
+  const [showLinks, setShowLinks] = useState(false);
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  console.log(showLinks);
+  return (
+    <header>
+      <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+        <ul className="navbar__links">
+          <li className="navbar__item">
+            <a href="/" className="navbar__link">
+              hOmE
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="/profil" className="navbar__link">
+              prOfil
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="/contact" className="navbar__link">
+              cOntAct
+            </a>
+          </li>
+        </ul>
+        <button className="navbar__burger" onClick={handleShowLinks}>
+          <span className="burger-bar"></span>
+        </button>
+      </nav>
+    </header>
+  );
+}
+export default Header;
