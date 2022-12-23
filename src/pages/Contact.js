682bf8998ef18c46e5fc37bd870496be239a9bc8
jeremybeziat <jeremybeziat.pro@gmail.com>
2022-12-23 16:55:37 +0100

    refactor : pages

---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Profil() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <h1>jErEmy bEziAt</h1>
+      </main>
+    </div>
+  );
+}
+export default Profil;
---
+++ src/pages/Home.js
@@ -0,0 +1,100 @@
+import React from "react";
+import Header from "../components/Header";
+import works from "../assets/images/bgd.webp";
+import works2 from "../assets/images/rainingstars.jpg";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section className="presentation">
+          <div className="name">
+            <h1>jErEmy bEziAt</h1>
+            <a href="/">
+              <svg
+                width="22"
+                height="22"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15.4454 10.9099L12.9264 10.8657L12.9264 4.59014L2.18718 15.3293L0.419417 13.5616L11.1586 2.82237L4.88303 2.82237L4.83883 0.303301H15.4454V10.9099Z"
+                  fill="white"
+                  fill-opacity="0.6"
+                />
+              </svg>
+            </a>
+          </div>
+          <div>
+            <h2>wEb dEvElOpEr</h2>
+            <hr />
+            <h2>dEsignEr ui</h2>
+            <hr />
+            <h2>grAphic dEsignEr</h2>
+          </div>
+        </section>
+        <section className="works">
+          <h2>wOrks</h2>
+          <hr />
+          <figure>
+            <img src={works} alt="works" />
+            <a href="/">
+              <figcaption>
+                <h3>prOjEcts</h3>
+                <svg
+                  width="39"
+                  height="39"
+                  viewBox="0 0 39 39"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="19.5"
+                    cy="19.5"
+                    r="18.525"
+                    stroke="white"
+                    stroke-width="1.95"
+                  />
+                  <path
+                    d="M22.037 26.4444L20.3519 24.6991L24.625 20.4259H10V18.0185H24.625L20.3519 13.7454L22.037 12L29.2593 19.2222L22.037 26.4444Z"
+                    fill="white"
+                  />
+                </svg>
+              </figcaption>
+            </a>
+          </figure>
+          <figure>
+            <img src={works2} alt="works" />
+            <a href="/">
+              <figcaption>
+                <h3>prOjEcts</h3>
+                <svg
+                  width="39"
+                  height="39"
+                  viewBox="0 0 39 39"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle
+                    cx="19.5"
+                    cy="19.5"
+                    r="18.525"
+                    stroke="white"
+                    stroke-width="1.95"
+                  />
+                  <path
+                    d="M22.037 26.4444L20.3519 24.6991L24.625 20.4259H10V18.0185H24.625L20.3519 13.7454L22.037 12L29.2593 19.2222L22.037 26.4444Z"
+                    fill="white"
+                  />
+                </svg>
+              </figcaption>
+            </a>
+          </figure>
+        </section>
+      </main>
+    </div>
+  );
+}
+export default Home;
