638c2fa862be8ac45b8eb4ccff1cf65f03679936
jeremybeziat <jeremybeziat.pro@gmail.com>
2023-04-13 16:12:57 +0200

    refactor : other works

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
 
 function App() {
+  useEffect(() => {
+    // Utiliser gsap pour créer une animation de défilement sur window
+    gsap.to(window, {
+      scrollTop: 0, // La position de défilement finale
+      duration: 5, // La durée de l'animation en secondes
+      delay: 2, // Le délai avant que l'animation ne commence en secondes
+      ease: "power1.out", // L'animation d'accélération/décélération
+    });
+  }, []);
   return (
     <BrowserRouter>
         <Routes>
